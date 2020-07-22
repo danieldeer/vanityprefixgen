c7c0bbcf0764174ff5a2b60a67506013a6e7bee2
unknown <dont@mind.com>
2020-07-22 23:20:57 +0100

    Implement the word generation process

--- btcprefixgen.sh
+++ btcprefixgen.sh
@@ -20,3 +20,26 @@ done
 
 echo "Out: $OUTPUTFILE"
 echo "In: $INPUTFILE"
+
+rm temp tempLowercase tempFirstLetterUppercase 
+#make all lines lowercase
+sed 's/.*/\L&/g' $INPUTFILE > tempLowercase
+
+#now make first character of each line uppercase and store in another temp file
+sed 's/./\u&/' tempLowercase > tempFirstLetterUppercase
+
+#append firstLetterUppercase file to the lowercase file
+cat tempFirstLetterUppercase >> tempLowercase
+
+#Prepend the prefix '1' specific to BTC public addresses to each line
+sed -e 's/^/1/' tempLowercase > temp
+
+#From inputfile: remove all lines with invalid characters and store in temp file
+sed -E '/l|I|O|0/d' temp > $OUTPUTFILE
+
+#remove temporary files
+rm temp tempLowercase tempFirstLetterUppercase
+
+
+
+
